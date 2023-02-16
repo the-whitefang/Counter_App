46997e89404ca4e0aedbe7ac3712a5b42a2e5f1b
Abhilash <abhilashvenugopal11111@gmail.com>
2023-02-16 11:25:00 +0530

    few changes added in the MainActivity file

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -3,27 +3,30 @@ package com.example.counter_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.counter_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         binding.btn1.setOnClickListener {
             var value1 = binding.value1.text.toString()
             var value2 = binding.value2.text.toString()
-            if(value1.isNotEmpty() && value2.isNotEmpty()){
-                var A = value1.toInt()
-                var B = value2.toInt()
-                binding.result.text = (A+B).toString()
+            if (value1.isEmpty() && value2.isEmpty()){
+                Toast.makeText(this,"Please enter any value to perform the Addition"
+                ,Toast.LENGTH_SHORT).show()
             }
             else{
-                Toast.makeText(this,"Please enter any to Add.",Toast.LENGTH_SHORT).show()
+                var num1 = value1.toInt()
+                var num2 = value2.toInt()
+                binding.result.text = (num1+num2).toString()
             }
         }
+
+
 
 
     }
